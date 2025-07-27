3a37b58c73faa40b87b0c4b16ab95e2701f6da63
Jaehee Roh <snowcat3b@gmail.com>
2025-07-27 20:14:53 +0900

    study3-2

--- study3/main.js
+++ study3/main.js
@@ -4,5 +4,5 @@ function showPrice(){
     var savedPrice = originalPrice * (rate/100);
     var resultPrice = originalPrice - savedPrice;
     document.querySelector("#result").innerHTML = `상품의 원래 가격은 ${originalPrice}원이고, 할인율은 ${rate}%입니다.`
-    document.querySelector("#cost-result").innerHTML = `할인된 가격은 ${savedPrice}원으로 ${resultPrice}원에 구매하실수 있습니다`
+    document.querySelector("#cost-result").innerHTML = `할인받는 가격은 ${savedPrice}원으로 ${resultPrice}원에 구매하실수 있습니다`
 }
