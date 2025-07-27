e8e303d35907ef5f4943ade6d2bea9326fff7988
Jaehee Roh <snowcat3b@gmail.com>
2025-07-27 20:31:50 +0900

    study2-1

--- study2/main.js
+++ study2/main.js
@@ -1,9 +1,9 @@
 function calc(){
-    var currentYear = prompt("올해 년도를 입력하세요", "YYYY");
-    var birthYear = prompt("태어난 년도를 입력하세요", "YYYY");
+    var currentYear = prompt("현재 년도를 입력하시오");
+    var birthYear = prompt("태어난 년도를 입력하시오");
     var age = currentYear-birthYear+1;
-    
-    document.querySelector("#result").innerHTML= "당신의 나이는" + age +"세 입니다."
+    // alert(`당신의 나이는 ${age} 입니다`);
+    document.querySelector("#result").innerHTML = (`당신의 나이는 ${age} 입니다`);
 }
 
 //let : 지역변수. {}밖으로 나가면 사용할수없다.
