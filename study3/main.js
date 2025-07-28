bd1e3ea9574a0efc4c9d110d754b6792ad121a23
Jaehee Roh <snowcat3b@gmail.com>
2025-07-28 20:29:45 +0900

    study3-3

--- study3/main.js
+++ study3/main.js
@@ -1,9 +1,9 @@
 function calc(){
-    var originPrice = document.querySelector("originPrice").value;
+    var originPrice = document.querySelector("#originPrice").value;
     var rate = document.querySelector("#rate").value;
     if(originPrice && rate){
         var savedPrice = originPrice * (rate/100);
         var resultPrice = originPrice - savedPrice;
-        document.querySelector("#showResult").innerHTML = `${savedPrice}원 할인되어 ${resultPrice}원에 구매하실수 있습니다`
+        document.querySelector("#showResult").innerHTML = `${savedPrice}원 할인되어 ${resultPrice}원에 구매하실수 있습니다`;
     }
 }
