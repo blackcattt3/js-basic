4c468bb7e64466f8da7a9c9301fa0a68b641fbb6
Jaehee Roh <snowcat3b@gmail.com>
2025-08-03 20:21:25 +0900

    quiz5-1

--- quiz/quiz5-1/main.js
+++ quiz/quiz5-1/main.js
@@ -22,24 +22,32 @@
 // 첫번째 버튼 눌렀을때 인자 받기 시작
 // 두번째 버튼 눌렀을때 결과값 반환
 
-var getVar = document.getElementById("btn1");
-var showResult = document.getElementById("btn2");
 
-getVar.onclick = function(){
-    document.querySelector("#number").innerHTML = calc();
-}
-showResult.addEventListener("click",function(){
-    document.querySelector("#result").innerHTML = add(num1, num2);
-});
-function calc(){
-    num1 = Number(prompt("첫번째 숫자"));
-    num2 = Number(prompt("두번째 숫자"));
-    return `입력된 숫자 : ${num1}과 ${num2}`;
-}
 
-function add(a,b){
-    return a+b;
-}
+
+// var getVar = document.getElementById("btn1");
+// var showResult = document.getElementById("btn2");
+
+// getVar.onclick = function(){
+//     document.querySelector("#number").innerHTML = calc();
+// }
+// // getVar.addEventListener("click", calc);
+// showResult.addEventListener("click",function(){
+//     document.querySelector("#result").innerHTML = add(num1, num2);
+// });
+// // showResult.addEventListener("click", add(a,b))
+// function calc(){
+//     num1 = Number(prompt("첫번째 숫자"));
+//     num2 = Number(prompt("두번째 숫자"));
+//     return `입력된 숫자 : ${num1}과 ${num2}`;
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+
+
+
 
 // resetButton.addEventListener("click", reset);
 // 이벤트 핸들러를 등록하는 코드가 아니라 calc()를 즉시 실행하는 코드였다. calc()는 함수니까 ()를 붙이면 즉시 실행된다.
@@ -57,10 +65,30 @@ function add(a,b){
 // 함수() -> 즉시 실행
 // 함수 -> 이벤트로 전달(필요할때 실행됨)
 
-혹은 addEventListener 사용하기
+// 혹은 addEventListener 사용하기
 
-getVar.addEventListener("click", calc()); // ❌ calc()는 즉시 실행됨
+// getVar.addEventListener("click", calc()); // ❌ calc()는 즉시 실행됨
 
-//올바른 문법
-getVar.addEventListener("click", ()=>{document.getElementById("number").innerHTML = calc();});
-getVar.addEventListener("click", calc);
+// //올바른 문법
+// getVar.addEventListener("click", ()=>{document.getElementById("number").innerHTML = calc();});
+// getVar.addEventListener("click", calc);
+
+
+
+var getNumber = document.querySelector("#btn1");
+var showResult = document.getElementById("btn2");
+
+getNumber.onclick = calc;
+showResult.onclick = function(){
+    add(num1, num2);
+};
+
+function calc(){
+    num1 = Number(prompt("첫번째 숫자를 입력하세요"));
+    num2 = Number(prompt("두번째 숫자를 입력하세요"));
+    document.querySelector("#number").innerHTML = `${num1}와 ${num2}`;
+}
+
+function add(a,b){
+    document.querySelector("#result").innerHTML = `두수의 합은 ${a+b} 입니다`;
+}
