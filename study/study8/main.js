3fd9eac4596d33a06c5bae85e7a28902bf910236
Jaehee Roh <snowcat3b@gmail.com>
2025-08-18 20:31:24 +0900

    study8 finish

--- study/study8/main.js
+++ study/study8/main.js
@@ -42,33 +42,101 @@
 //     }
 // })
 
+// function newRegister(){
+//     let newP = document.createElement("p");
+//     let userName = document.querySelector("#userName");
+//     let newText = document.createTextNode(userName.value);
+//     newP.appendChild(newText);
+
+//     let delBttn = document.createElement("span");
+//     let delText = document.createTextNode("X");
+//     delBttn.appendChild(delText);
+//     // let delAttr = document.createAttribute("class");
+//     // delAttr.value = "del";
+//     // delBttn.setAttributeNode(delAttr);
+//     delBttn.setAttribute("class", "del");
+//     newP.appendChild(delBttn);
+
+//     let nameList = document.querySelector("#nameList");
+//     // nameList.appendChild(newP);
+//     nameList.insertBefore(newP,nameList.childNodes[0]);
+//     userName.value = "";
+
+//     let removeBttns = document.querySelectorAll(".del");
+
+//     for(let i=0;i<removeBttns.length;i++){
+//         removeBttns[i].addEventListener("click", function(){
+//             if(this.parentNode.parentNode){
+//                 this.parentNode.parentNode.removeChild(this.parentNode);
+//             }
+//         });
+//     }
+// }
+
+
+// let bigPic = document.querySelector("#cup");
+// let smallPic = document.querySelectorAll(".small");
+
+// for(let i=0;i<smallPic.length;i++){
+//     smallPic[i].addEventListener("click", function(){
+//         bigPic.src = this.src
+//     });
+// }
+
+
+// function newRegister(){
+//     let newP = document.createElement("p");
+//     let userName = document.querySelector("#user-name");
+//     let newText = document.createTextNode(userName.value);
+//     newP.appendChild(newText);
+
+//     let newS = document.createElement("span");
+//     let sText = document.createTextNode("X");
+//     newS.appendChild(sText);
+//     newS.setAttribute("class", "del");
+//     newP.appendChild(newS);
+
+//     let nameList = document.querySelector("#name-list");
+//     nameList.insertBefore(newP, nameList.children[0]);
+//     userName.value = "";
+
+//     var removeBtn = document.querySelectorAll(".del");
+//     for(let i=0;i<removeBtn.length;i++){
+//         removeBtn[i].addEventListener("click", function(){
+//             if(this.parentNode.parentNode){
+//                 this.parentNode.parentNode.removeChild(this.parentNode);
+//             }
+//         })
+//     }
+// }
+
+
+
 function newRegister(){
     let newP = document.createElement("p");
-    let userName = document.querySelector("#userName");
+    let userName = document.querySelector("#user-name");
     let newText = document.createTextNode(userName.value);
     newP.appendChild(newText);
 
-    let delBttn = document.createElement("span");
-    let delText = document.createTextNode("X");
-    delBttn.appendChild(delText);
-    // let delAttr = document.createAttribute("class");
-    // delAttr.value = "del";
-    // delBttn.setAttributeNode(delAttr);
-    delBttn.setAttribute("class", "del");
-    newP.appendChild(delBttn);
+    let newS = document.createElement("span");
+    let sText = document.createTextNode("X");
+    newS.setAttribute("class", "del");
+    newS.appendChild(sText);
+    newP.appendChild(newS);
 
-    let nameList = document.querySelector("#nameList");
-    // nameList.appendChild(newP);
-    nameList.insertBefore(newP,nameList.childNodes[0]);
-    userName.value = "";
+    let nameList = document.querySelector("#name-list");
+    nameList.insertBefore(newP,nameList.children[0]);
+    userName.value = ""
 
-    let removeBttns = document.querySelectorAll(".del");
-
-    for(let i=0;i<removeBttns.length;i++){
-        removeBttns[i].addEventListener("click", function(){
+    let delBtn = document.querySelectorAll(".del");
+    for(let i=0;i<delBtn.length;i++){
+        delBtn[i].addEventListener("click", function(){
             if(this.parentNode.parentNode){
                 this.parentNode.parentNode.removeChild(this.parentNode);
             }
-        });
+            // this -> span
+            // this.parentNode -> p
+            // this.parentNode.parentNode -> name-list
+        })
     }
 }
