4877fe239b1d0299c1693f57c7c2fccb8f02850e
Jaehee Roh <snowcat3b@gmail.com>
2025-08-21 20:59:54 +0900

    quiz9-1 edit

--- quiz/quiz9-1/main.js
+++ quiz/quiz9-1/main.js
@@ -1,11 +1,19 @@
 let basicPrice = 24000;
 let items = document.querySelectorAll(".checkbx");
 
-for(let i=0;i<items.length;i++){
-    items[i].addEventListener("click", function(){
-        let price =Number(this.value);
-        basicPrice += this.checked? price: -price;
+items.forEach(item =>{
+    item.addEventListener("click", function(){
+        let price = Number(this.value);
+        basicPrice += this.checked? price : -price;
         document.querySelector(".price").value = `${basicPrice}`;
-    });
-}
+    })
+})
 
+
+// for(let i=0;i<items.length;i++){
+//     items[i].addEventListener("click", function(){
+//         let price = Number(this.value);
+//         basicPrice += this.checked? price : -price;
+//         document.querySelector("#total").value = basicPrice;
+//     })
+// }
