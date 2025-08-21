0db816ef80172cb64b49c30a639e6da7b3b2bc66
Jaehee Roh <snowcat3b@gmail.com>
2025-08-21 14:29:15 +0900

    study9

--- study/study9/main.js
+++ study/study9/main.js
@@ -43,3 +43,8 @@
 // }
 
 
+let selectMenu = document.testForm.major;
+function displaySelect(){
+    let selectedText = selectMenu.options[selectMenu.selectedIndex].innerText;
+    alert(`${selectedText}를 선택했습니다`);
+}
